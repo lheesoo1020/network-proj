dfe4ab18c670e586a574cbf9a36f8845ff291478
lheesoo1020 <lheesoo1020@gmail.com>
2024-04-06 23:42:06 +0900

    server1

--- proj1/myserver.c
+++ proj1/myserver.c
@@ -4,9 +4,84 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main() {
-    const char* server_ip = "127.0.0.1";
-    int server_port = 9152;
+
+#define BUFFER_SIZE 1024
+
+
+char* getFileName(int i) {
+	char* fileName;
+	switch(i) {
+		case 1:
+			fileName = "1.jpg";
+			break;
+		default:
+			fileName = "";
+			break;
+	}
+	return fileName;
+}
+
+
+void sendFile(int client_socket, char* filename) {
+	char buffer[BUFFER_SIZE];
+	size_t bytesRead;
+	
+	FILE *file = fopen(filename, "rb");
+	if (file == NULL) {
+		perror("Opening file failed");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
+		send(client_socket, buffer, bytesRead, 0);
+	}
+	fclose(file);
+}
+
+
+void sendResponse(int client_socket, int i) {
+	char* response;
+	char* fileName;
+	long fsize;
+	char buffer[BUFFER_SIZE];
+	size_t bytesRead;
+	printf("111");
+	fileName = getFileName(i);
+
+	FILE *file = fopen(fileName, "rb");
+	if (file == NULL) {
+		perror("Opening file failed");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("1");
+
+	fseek(file, 0, SEEK_END);
+	fsize = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	
+	printf("2");
+
+	sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nContent-Length: %ld\r\n\r\n", fsize);
+	
+	printf("3");
+
+	send(client_socket, response, sizeof(response), 0);
+
+	printf("4");
+
+	while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
+		send(client_socket, buffer, bytesRead, 0);
+	}
+
+	fclose(file);
+}
+
+
+int main(int argc, char* argv[]) {
+    const char* server_ip = "192.168.56.106";
+    int server_port = atoi(argv[1]);
+	char* fileName;
 
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
@@ -17,8 +92,8 @@ int main() {
     struct sockaddr_in server_addr, client_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(server_port);
-    server_addr.sin_addr.s_addr = inet_addr(server_ip);
-
+//    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         perror("Binding failed");
         return 1;
@@ -40,13 +115,19 @@ int main() {
         }
 
         printf("Accepted connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
+	
         char request[1024];
         recv(client_socket, request, sizeof(request), 0);
         printf("Received request:\n%s\n", request);
-
-        char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello, World!";
+		
+//        char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello, World!";
+		
+		char response[] = "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\n\r\n";
         send(client_socket, response, sizeof(response), 0);
+
+		fileName = getFileName(1);
+		sendFile(client_socket, fileName);
+//		sendResponse(client_socket, 1);
 
         close(client_socket);
     }
@@ -54,3 +135,4 @@ int main() {
     close(server_socket);
     return 0;
 }
+
