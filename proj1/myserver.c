5625c37ae1beffdf62d4d138dd146050df91d05d
lheesoo1020 <lheesoo1020@gmail.com>
2024-04-09 14:46:34 +0900

    fixed bug

--- proj1/myserver.c
+++ proj1/myserver.c
@@ -3,88 +3,27 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-
+#include <fcntl.h>
 
 #define BUFFER_SIZE 1024
 
 
-char* getFileName(int i) {
-	char* fileName;
-	switch(i) {
-		case 1:
-			fileName = "1.jpg";
-			break;
-		default:
-			fileName = "";
-			break;
-	}
-	return fileName;
-}
-
-
-void sendFile(int client_socket, char* filename) {
-	char buffer[BUFFER_SIZE];
-	size_t bytesRead;
-
-	FILE *file = fopen(filename, "rb");
-	if (file == NULL) {
-		perror("Opening file failed");
-		exit(EXIT_FAILURE);
-	}
-
-	while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
-		send(client_socket, buffer, bytesRead, 0);
-	}
-	fclose(file);
-}
-
-
-void sendResponse(int client_socket, int i) {
-	char* response;
-	char* fileName;
-	long fsize;
-	char buffer[BUFFER_SIZE];
-	size_t bytesRead;
-
-	fileName = getFileName(i);
-
-	FILE *file = fopen(fileName, "rb");
-	if (file == NULL) {
-		perror("Opening file failed");
-		exit(EXIT_FAILURE);
-	}
-
-	fseek(file, 0, SEEK_END);
-	fsize = ftell(file);
-	fseek(file, 0, SEEK_SET);
-	
-	sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nContent-Length: %ld\r\n\r\n", fsize);
-
-	send(client_socket, response, sizeof(response), 0);
-
-	while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
-		send(client_socket, buffer, bytesRead, 0);
-	}
-
-	fclose(file);
-}
-
 void requestHandle(int client_socket, char* request) {
-	char method[10], path[20];
-	FILE* file;
-	long fsize;
-	int option;
-	char buffer[BUFFER_SIZE];
-	char response[BUFFER_SIZE];
-	size_t bytesRead;
+    char method[10], path[20];
+    FILE* file;
+    long fsize;
+    int option;
+    char buffer[BUFFER_SIZE];
+    char response[BUFFER_SIZE];
+    size_t bytesRead;
 
-	sscanf(request, "%s %s", method, path);
+    sscanf(request, "%s %s", method, path);
 
-	if (strcmp(path, "/")) {
+	if (!strcmp(path, "/")) {
 		file = fopen("index.html", "rb");
 		option = 0;
 	}
-	else if (strcmp(path, "/1")) {
+	else if (!strcmp(path, "/1")) {
 		file = fopen("1.jpg", "rb");
 		option = 1;
 	}
@@ -107,8 +46,7 @@ void requestHandle(int client_socket, char* request) {
 			break;
 	}
 
-	send(client_socket, response, sizeof(response), 0);
-	
+	send(client_socket, response, strlen(response), 0);	
 	while ((bytesRead = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
 		send(client_socket, buffer, bytesRead, 0);
 	}
@@ -118,9 +56,8 @@ void requestHandle(int client_socket, char* request) {
 }
 
 int main(int argc, char* argv[]) {
-    const char* server_ip = "192.168.56.106";
+    const char* server_ip = "10.0.2.15";
     int server_port = atoi(argv[1]);
-	char* fileName;
 
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
@@ -131,8 +68,7 @@ int main(int argc, char* argv[]) {
     struct sockaddr_in server_addr, client_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(server_port);
-//    server_addr.sin_addr.s_addr = inet_addr(server_ip);
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         perror("Binding failed");
         return 1;
@@ -158,16 +94,8 @@ int main(int argc, char* argv[]) {
         char request[1024];
         recv(client_socket, request, sizeof(request), 0);
         printf("Received request:\n%s\n", request);
-		
-//      char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello, World!";
-		
-//		char response[] = "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\n\r\n";
-//      send(client_socket, response, sizeof(response), 0);
 
-//		fileName = getFileName(1);
-//		sendFile(client_socket, fileName);
-//		sendResponse(client_socket, 1);
-		requestHandle(client_socket, request);
+        requestHandle(client_socket, request);
 
         close(client_socket);
     }
